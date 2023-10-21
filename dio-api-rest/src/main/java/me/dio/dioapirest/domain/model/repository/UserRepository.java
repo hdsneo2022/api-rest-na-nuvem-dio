cde47e7f590519a651d6b651f6f4cfe3b0e8fb20
Haron Santiago <hdsneo2014@gmail.com>
2023-10-20 21:04:52 -0300

    implementação da camada de dominio

---
+++ dio-api-rest/src/main/java/me/dio/dioapirest/domain/model/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package me.dio.dioapirest.domain.model.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import me.dio.dioapirest.domain.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    
+    boolean existsByAccountNumber(String number);
+
+    boolean existsByCardNumber(String number);
+}
---
+++ dio-api-rest/src/test/java/me/dio/dioapirest/DioApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.dioapirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
